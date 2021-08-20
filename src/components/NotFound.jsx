1aa4bbf5c9bd59a6ac26f80f0d481a90eb921d82
Angel <zaggel2002@gmail.com>
2021-08-20 01:18:53 -0500

    Redireccionamiento del componente NOENCONTRADO a Inicio

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
     <div>
       PAGE NOT FOUND
+      <Link to="/">Inicio</Link>
+      <Link to="/loginpage">Registro</Link>
     </div>
   )
 }
